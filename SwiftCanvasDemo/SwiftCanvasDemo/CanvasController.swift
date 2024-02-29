7b88f6681ef26db02e0692eb9c4a4c6c49532efd
Andre Pham <andrepham33@gmail.com>
2024-02-29 17:53:06 +1100

    Created canvas controller

---
+++ SwiftCanvasDemo/SwiftCanvasDemo/CanvasController.swift
@@ -0,0 +1,19 @@
+//
+//  CanvasController.swift
+//  SwiftCanvasDemo
+//
+//  Created by Andre Pham on 29/2/2024.
+//
+
+import Foundation
+import UIKit
+
+class CanvasController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .red
+    }
+    
+}
--- SwiftCanvasDemo/SwiftCanvasDemo/ViewController.swift
+++ SwiftCanvasDemo/SwiftCanvasDemo/ViewController.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let canvasController = CanvasController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Step 1: Add the Child View Controller to the Parent
+        addChild(self.canvasController)
+        
+        // Step 2: Set Up the Child View Controller’s View
+        view.addSubview(self.canvasController.view)
+        self.canvasController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Step 3: Apply Auto Layout Constraints
+        NSLayoutConstraint.activate([
+            self.canvasController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            self.canvasController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+            self.canvasController.view.topAnchor.constraint(equalTo: view.topAnchor, constant: 64),
+            self.canvasController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -48)
+        ])
+        
+        // Step 4: Notify the Child View Controller
+        self.canvasController.didMove(toParent: self)
     }
 
 
