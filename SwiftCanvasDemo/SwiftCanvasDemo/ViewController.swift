e414ac5da46bb6d910962001c8961b8cc8faed57
Andre Pham <andrepham33@gmail.com>
2024-02-29 17:41:14 +1100

    Demo init

---
+++ SwiftCanvasDemo/SwiftCanvasDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftCanvasDemo
+//
+//  Created by Andre Pham on 29/2/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
